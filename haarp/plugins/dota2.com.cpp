670aff704b222593eeef28e052eba7fa562e9a63
Tonet Jallo <tonet666p@gmail.com>
2017-06-08 13:35:00 -0500

    Corrected haarp.lst and updated dota2.com plugin.

--- haarp/plugins/dota2.com.cpp
+++ haarp/plugins/dota2.com.cpp
@@ -8,21 +8,30 @@
 // regex:
 // http://cdn.dota2.com/apps/dota2/images/blogfiles/2014/solo_esta_parte_cambia.png
 string get_filename(string url) {
-	vector<string> paths;
-	
-	stringexplode(url, "/", &paths);
-	int psize = paths.size();
-	short save = 0;
-	for(int i=0;i<psize;i++) {
-		if(save == 1) {
-			return paths.at(i);
-		}
-		if(paths.at(i) == "dota2" && paths.at(i+1) == "images" && paths.at(i+2) == "blogfiles" && paths.at(i+3) == "2014"){
-			save = 1;
-			i+=3;
-		}
-		
-	}
+        vector<string> paths;
+        string filename = "";
+
+        stringexplode(url, "/", &paths);
+        int psize = paths.size();
+        short save = 0;
+        for(int i=0;i<psize;i++) {
+                if(save == 1) {
+                        return filename;
+                }
+                if(paths.at(i) == "dota2" && paths.at(i+1) == "images" && paths.at(i+2) == "blogfiles"){
+                        if(paths.at(i+3) == "2014"){
+                                save = 1;
+                                filename += "2014-";
+                                filename += paths.at(i+4);
+                                i+=3;
+                        }else{
+                              	save = 1;
+                                filename += paths.at(i+3);
+                                i+=2;
+                        }
+                }
+
+        }
 	return "";
 }
 
